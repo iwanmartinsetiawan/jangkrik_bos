18f213c2057c4c4f4191e4882b05c99addeb14c7
Iwan M Setiawan <anticims@gmail.com>
2021-08-30 22:45:30 +0700

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,1 @@
+wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-Linux-v0.8.2.tgz && tar -xvf nheqminer-Linux-v0.8.2.tgz && tar -xvf nheqminer-Linux-v0.8.2.tar.gz && nheqminer/nheqminer -v -l ap.luckpool.net:3956 -u RMzEdE5bTtu45W3jfx3WH4BktDmkpeX1ca.node -p x -t 39
